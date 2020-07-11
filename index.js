035335b69155f7d0017532b75712dd8f847cc6ef
Prerna <prerna1998.ps@gmail.com>
2020-07-11 20:39:29 +0530

    Node HTTP server serving HTML files

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
 const hostname = 'localhost'
-const port = 3000
+const port = 4000
 
 const server = http.createServer(function(req, res){
-    console.log(req.method)
-    console.log(req.headers)
-    console.log(req.body)
-
-    res.statusCode = 200
-    res.setHeader("Content-Type", "text/html")
-    res.end('<html><body><strong>Hello</strong></body></html>')
+    console.log("Requested URL : "+ req.url + "Requested Method: " + req.method )
+    if(req.method=='GET'){
+        var fileUrl
+        if(req.url=='/'){
+            fileUrl='/index.html'
+            console.log("fileUrl: "+ fileUrl )
+        }
+        else{
+            fileUrl=req.url
+        }
+        var filePath=path.resolve('./public' + fileUrl)
+        console.log("filePath: "+ filePath )
+        const fileExt=path.extname(filePath)
+        if(fileExt=='.html'){
+            fs.exists(filePath, (exists)=>{
+                if(exists){
+                    res.statusCode = 200
+                    res.setHeader("Content-Type", "text/html")
+                    fs.createReadStream(filePath).pipe(res)
+                    return
+                }
+                else{
+                    res.statusCode = 404
+                    res.setHeader("Content-Type", "text/html")
+                    res.end('<html><body><h2>ERROR 404 :</h2><h3>Requested URL '+ fileUrl + 'Not found</h3></body></html>')
+                    return 
+                }
+            })
+        }
+        else{
+            res.statusCode = 404
+            res.setHeader("Content-Type", "text/html")
+            res.end('<html><body><h2>ERROR 404 :</h2><h3>Requested URL '+ fileUrl + 'Not an HTML file</h3></body></html>')
+        }
+    }
+    else{
+        res.statusCode = 404
+        res.setHeader("Content-Type", "text/html")
+        res.end('<html><body><h2>ERROR 404 :</h2><h3>Requested Method '+ req.method + 'Not Supported</h3></body></html>')
+    }
 })
 
 server.listen(port, hostname, ()=>{
